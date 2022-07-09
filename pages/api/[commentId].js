914f22544e2aca6c579a9b8cd46819c7896e68b0
Dane Borg <bogdan.cirstea.en@gmail.com>
2022-07-09 23:53:33 +0100

    constructing the logic to coolect and display the comments on each page!

--- pages/api/[commentId].js
+++ pages/api/[commentId].js
@@ -3,6 +3,12 @@ import path from "path";
 
 function handler(req, res) {
   const commentId = req.query.commentId;
+
+  if (req.method === "POST") {
+    // then take to info provided and send the comment into the collection of comments for this specific page
+  } else if (req.method === "GET") {
+    // then get the comments for this specific page and display the comments
+  }
 }
 
 export default handler;
