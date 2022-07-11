5c0924d34a2df8ba243cdf2b42d09c6a3d8678dc
Dane Borg <bogdan.cirstea.en@gmail.com>
2022-07-12 00:00:32 +0100

    writing the new comment api route!

--- pages/api/[commentsPageId].js
+++ pages/api/[commentsPageId].js
@@ -5,6 +5,7 @@ function handler(req, res) {
   const commentsPageId = req.query.commentsPageId;
 
   if (req.method === "POST") {
+    const filePath = path.join(process.cwd);
     // then take to info provided and send the comment into the collection of comments for this specific page
   } else if (req.method === "GET") {
     // then get the comments for this specific page and display the comments
