56eaae8284e1d2b74f88d8b3458d3c8ad59c9367
Dane Borg <bogdan.cirstea.en@gmail.com>
2022-07-14 00:20:08 +0100

    progress in writing the api route for post and get requests for collecting and displaying the comments

--- pages/api/[commentsPageId].js
+++ pages/api/[commentsPageId].js
@@ -4,10 +4,32 @@ import path from "path";
 function handler(req, res) {
   const commentsPageId = req.query.commentsPageId;
 
+  const email = req.body.email;
+  const name = req.body.name;
+  const commentText = req.body.commentText;
+
+  const newComment = {
+    id: new Date().toISOString(),
+    email: email,
+    name: name,
+    commentText: commentText,
+  };
+
   if (req.method === "POST") {
-    const filePath = path.join(process.cwd);
+    const filePath = path.join(process.cwd(), "data", "comments.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newComment);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res
+      .status(201)
+      .json({ message: "New comment recorded!", comment: newComment });
     // then take to info provided and send the comment into the collection of comments for this specific page
   } else if (req.method === "GET") {
+    const filePath = path.join(process.cwd(), "data", "comments.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    res.status(200).json({ comment: data });
     // then get the comments for this specific page and display the comments
   }
 }
